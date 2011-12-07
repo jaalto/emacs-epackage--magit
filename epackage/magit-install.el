ce854b6d0a68dcd75af230711812e4519a27b7a4
Jari Aalto <jari.aalto@cante.net>
2011-12-07 11:37:15 +0200

    epackage/: Change provide to *-epkg-*. Fix xactivate, install
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/magit-install.el
+++ epackage/magit-install.el
@@ -1,3 +1,2 @@
 (autoload 'magit-status "magit" "" t)
-(global-set-key "\C-cvv" 'magit-status)	;Like Standard Emacs C-x v v
-(provide 'magit-xactivate)
+(provide 'magit-epkg-install)
--- epackage/magit-xactivate.el
+++ epackage/magit-xactivate.el
@@ -1,2 +1,3 @@
-
-(provide 'magit-install)
+(require 'magit-epkg-autoloads)
+(global-set-key "\C-cvv" 'magit-status)	;Like Standard Emacs C-x v v
+(provide 'magit-epkg-xactivate)
